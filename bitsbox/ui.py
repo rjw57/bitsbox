33fdd9b0a42ebe8f4f6cf16dd02231433bbd2583
Rich Wareham <rjw57@cam.ac.uk>
2017-01-10 16:32:08 +0000

    tidy up

--- bitsbox/ui.py
+++ bitsbox/ui.py
@@ -10,8 +10,13 @@ blueprint = Blueprint('ui', __name__)
 def index():
     db = get_db()
     cabinets = db.execute(
-        'SELECT name, layout AS "layout [json]" FROM cabinets')
-    return render_template('cabinets.html', cabinets=cabinets)
+        'SELECT id, name, layout FROM cabinets ORDER BY name, id')
+    drawers = dict((row['key'], row) for row in db.execute('''
+        SELECT id, cabinet_id || "." || location AS key, label
+        FROM drawers
+    '''))
+    return render_template(
+        'cabinets.html', cabinets=cabinets, drawers=drawers)
 
 @blueprint.app_template_filter('fromjson')
 def fromjson_filter(s):
