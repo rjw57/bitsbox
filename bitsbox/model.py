449555b05aa1838469dbd5206939638717f9049d
Rich Wareham <rjw57@cam.ac.uk>
2017-01-11 16:06:55 +0000

    start to wire in functionality

--- bitsbox/model.py
+++ bitsbox/model.py
@@ -1,3 +1,5 @@
+from collections import deque
+import copy
 import json
 import sqlite3
 
@@ -5,6 +7,8 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 import yaml
+
+from ._util import token_urlsafe
 
 def connect_db():
     """Connects to the application database."""
@@ -35,37 +39,116 @@ def init_db():
 
 cli = click.Group('model', help='Manipulates the database.')
 
+def create_cabinet(cursor, name, layout_id):
+    """Creates a new cabinet with the specified name and layout id.
+    Automatically creates one drawer for each item of the layout spec.
+
+    Returns: rowid of newly created cabinet.
+
+    """
+    cursor.execute(
+        'INSERT INTO cabinets (name, layout_id) VALUES (?, ?)',
+        (name, layout_id))
+    cabinet_id = cursor.lastrowid
+    assert cabinet_id is not None and cabinet_id > 0
+
+    # Create a new location for each item in the layout spec
+    cursor.execute('''
+        INSERT INTO
+            locations (cabinet_id, layout_item_id)
+        SELECT
+            cabinets.id, layout_items.id
+        FROM
+            cabinets JOIN layout_items
+        ON
+            cabinets.layout_id = layout_items.layout_id
+        WHERE
+            cabinets.id = ?
+    ''', (cabinet_id,))
+
+    # Create a new drawer for each location
+    cursor.execute('''
+        INSERT INTO
+            drawers (label, location_id)
+        SELECT
+            (
+                SELECT
+                    group_concat(value, '.')
+                FROM
+                    json_each(layout_items.spec_item_path)
+            ),
+            locations.id
+        FROM
+            locations JOIN layout_items
+        ON
+            locations.layout_item_id = layout_items.id
+        WHERE
+            locations.cabinet_id = ?
+    ''', (cabinet_id,))
+
+
+    return cabinet_id
+
 @cli.command('initdb')
 @with_appcontext
 def initdb_command():
     """Initialises the database with the schema."""
     init_db()
 
-@cli.command('importcabs')
+@cli.command('addcabinet')
+@click.argument('name', type=str)
+@click.argument('layout_id', type=int)
+@with_appcontext
+def addcabinet_command(name, layout_id):
+    db = get_db()
+    cabinet_id = create_cabinet(db.cursor(), name, layout_id)
+    db.commit()
+    print('Created cabinet "{}" with id: {}'.format(name, cabinet_id))
+
+@cli.command('importlayouts')
 @click.argument('yaml_file', type=click.File('rb'))
 @with_appcontext
 def importcabs_command(yaml_file):
     db = get_db()
     cur = db.cursor()
 
-    cabinets = yaml.load(yaml_file).get('cabinets')
-    if cabinets is None:
-        raise RuntimeError('Input file has no "cabinets" key')
+    for layout in yaml.load(yaml_file).get('layouts', []):
+        name, spec = [layout[k] for k in 'name spec'.split()]
+        existing_layout_id = cur.execute('''
+            SELECT 1 FROM layouts WHERE name = ? LIMIT 1
+        ''', (name,))
+        if existing_layout_id.fetchone() is not None:
+            print('Skipping existing layout: {}'.format(name))
+            continue
 
-    for cabinet in cabinets:
-        name, layout = [cabinet[k] for k in 'name layout'.split()]
+        # Insert layout into DB
         cur.execute(
-            'INSERT INTO cabinets (name, layout) VALUES (?, ?)',
-            (name, json.dumps(layout)))
-        cabinet_id = cur.lastrowid
+            'INSERT INTO layouts (name, spec) VALUES (?, ?)',
+            (name, json.dumps(spec)))
+        layout_id = cur.lastrowid
+        assert layout_id is not None and layout_id > 0
 
-        print('Importing cabinet {} ({})'.format(cabinet_id, name))
+        # Walk the spec and create values for each item for a layout_items row.
+        def walk_spec(spec, layout_id):
+            queue = deque([([], spec)]) # sequence of path/spec pairs
+            while len(queue) > 0:
+                path, item = queue.popleft()
+                type_ = item.get('type')
+                if type_ == 'container':
+                    queue.extend([
+                        (path + [idx], c)
+                        for idx, c in enumerate(item.get('children', []))])
+                elif type_ == 'item':
+                    yield json.dumps(path), layout_id
+                else:
+                    raise RuntimeError(
+                        'Unknown spec type: {}'.format(repr(type_)))
+
         cur.executemany(
-            'INSERT INTO drawers (label, cabinet_id, location) VALUES (?, ?, ?)',
-            [
-                ('{} {}'.format(name, loc), cabinet_id, loc)
-                for loc in layout_drawer_locations(layout)
-            ])
+            'INSERT INTO layout_items (spec_item_path, layout_id) VALUES (?, ?)',
+            walk_spec(spec, layout_id))
+
+        print('Importing layout "{}" with id: {}'.format(name, layout_id))
 
     db.commit()
 
--- bitsbox/ui.py
+++ bitsbox/ui.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort, request, redirect, url_for
 
 from .model import get_db
 
@@ -9,14 +9,153 @@ blueprint = Blueprint('ui', __name__)
 @blueprint.route('/')
 def index():
     db = get_db()
-    cabinets = db.execute(
-        'SELECT id, name, layout FROM cabinets ORDER BY name, id')
-    drawers = dict((row['key'], row) for row in db.execute('''
-        SELECT id, cabinet_id || "." || location AS key, label
-        FROM drawers
-    '''))
-    return render_template(
-        'cabinets.html', cabinets=cabinets, drawers=drawers)
+
+    context = {
+        'cabinets': db.execute('''
+            SELECT
+                cabinets.id AS id,
+                cabinets.name AS name,
+                layouts.spec AS layout
+            FROM
+                cabinets JOIN layouts
+            ON
+                cabinets.layout_id = layouts.id
+            ORDER BY
+                cabinets.name
+        '''),
+        'drawers_by_path': dict(
+            (row[0], row) for row in db.execute('''
+                SELECT
+                    (
+                        SELECT
+                            locations.cabinet_id || '.' || group_concat(value, '.')
+                        FROM
+                            json_each(layout_items.spec_item_path)
+                    ) AS path,
+                    drawers.id AS id,
+                    drawers.label AS label,
+                    IFNULL(
+                        (
+                            SELECT SUM(collections.contents_count)
+                            FROM collections
+                            WHERE collections.drawer_id = drawers.id
+                        ),
+                        0
+                    ) AS contents_count,
+                    (
+                        SELECT COUNT(1)
+                        FROM collections
+                        WHERE collections.drawer_id = drawers.id
+                    ) AS collections_count
+                FROM
+                    drawers
+                JOIN
+                    locations ON drawers.location_id = locations.id
+                JOIN
+                    layout_items ON locations.layout_item_id = layout_items.id
+                WHERE
+                    drawers.location_id IS NOT NULL
+            ''')
+        ),
+    }
+
+    return render_template('cabinets.html', **context)
+
+@blueprint.route('/drawer/<int:drawer_id>')
+def drawer(drawer_id):
+    db = get_db()
+
+    context = {
+        'drawer': db.execute('''
+            SELECT
+                drawers.id AS id,
+                drawers.label AS label
+            FROM
+                drawers
+            WHERE
+                drawers.id = ?
+        ''', (drawer_id,)).fetchone(),
+        'location': db.execute('''
+            SELECT
+                cabinets.id AS cabinet_id,
+                locations.id AS location_id,
+                cabinets.name AS cabinet_name,
+                layout_items.spec_item_path AS spec_item_path
+            FROM
+                drawers
+            JOIN
+                locations ON drawers.location_id = locations.id
+            JOIN
+                layout_items ON locations.layout_item_id = layout_items.id
+            JOIN
+                cabinets ON locations.cabinet_id = cabinets.id
+            WHERE
+                drawers.id = ?
+        ''', (drawer_id,)).fetchone(),
+        'collections': db.execute('''
+            SELECT
+                id AS collection_id,
+                name,
+                description,
+                contents_count
+            FROM
+                collections
+            WHERE
+                drawer_id = ?
+            ORDER BY
+                name, id
+        ''', (drawer_id,)).fetchall(),
+    }
+
+    if context['drawer'] is None:
+        abort(404)
+
+    return render_template('drawer.html', **context)
+
+@blueprint.route('/drawer/<int:drawer_id>/collections/new', methods=['GET', 'POST'])
+def drawer_collection_new(drawer_id):
+    db = get_db()
+
+    context = {
+        'drawer': db.execute('''
+            SELECT
+                drawers.id AS id,
+                drawers.label AS label
+            FROM
+                drawers
+            WHERE
+                drawers.id = ?
+        ''', (drawer_id,)).fetchone(),
+    }
+
+    if context['drawer'] is None:
+        abort(404)
+
+    # Handle simple GET request
+    if request.method == 'GET':
+        return render_template('collection_add.html', **context)
+
+    # This is the POST request, extract the form values
+    name = request.values.get('name', '')
+    if name == '':
+        abort(400)
+
+    description = request.values.get('description', '')
+    if description == '':
+        abort(400)
+
+    contents_count = int(request.values.get('contents_count', 0))
+
+    # Create the collection
+    db.execute('''
+        INSERT INTO
+            collections (name, description, contents_count, drawer_id)
+        VALUES
+            (?, ?, ?, ?)
+    ''', (name, description, contents_count, drawer_id))
+    db.commit()
+
+    return redirect(url_for('ui.drawer', drawer_id=drawer_id))
 
 @blueprint.app_template_filter('fromjson')
 def fromjson_filter(s):
