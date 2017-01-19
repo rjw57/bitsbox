1a897eb6af77ffa88e3de2eeea01586037f8d460
Rich Wareham <rich@richwareham.com>
2017-01-19 20:43:29 +0000

    add graphql

--- bitsbox/app.py
+++ bitsbox/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 
 from .ui import blueprint as ui
+from .graphql import (
+    graphql_blueprint as graphql,
+    graphiql_blueprint as graphiql
+)
 from .model import db
 from .cli import cli
 
@@ -15,9 +19,14 @@ def create_app(config_filename=None):
         app.config.from_pyfile(config_filename)
 
     db.init_app(app)
-    toolbar.init_app(app)
 
     app.register_blueprint(ui)
+    app.register_blueprint(graphql, url_prefix='/graphql')
     app.cli.add_command(cli)
+
+    # Things which should only be present in DEBUG-enabled apps
+    if app.debug:
+        app.register_blueprint(graphiql, url_prefix='/graphiql')
+        toolbar.init_app(app)
 
     return app
--- bitsbox/graphql.py
+++ bitsbox/graphql.py
@@ -1,15 +1,12 @@
+from flask import Blueprint, render_template
+from flask_graphql import GraphQLView
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 
-from .models import (
-    db_session, Layout as LayoutModel, Cabinet as CabinetModel
+from .model import (
+    db, Layout as LayoutModel, Cabinet as CabinetModel
 )
-
-class Layout(SQLAlchemyObjectType):
-    class Meta:
-        model = LayoutModel
-        interfaces = (relay.Node, )
 
 class Cabinet(SQLAlchemyObjectType):
     class Meta:
@@ -19,7 +16,18 @@ class Cabinet(SQLAlchemyObjectType):
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_cabinets = SQLAlchemyConnectionField(Cabinet)
-    all_layouts = SQLAlchemyConnectionField(Cabinet)
 
 schema = graphene.Schema(query=Query)
 
+graphql_blueprint = Blueprint('graphql', __name__)
+graphql_blueprint.add_url_rule(
+    '/', 'graphql', view_func=GraphQLView.as_view('graphql', schema=schema))
+
+graphiql_blueprint = Blueprint(
+    'graphiql', __name__, static_folder='static/graphiql',
+    static_url_path='/static',
+    template_folder='templates/graphiql')
+
+@graphiql_blueprint.route('/')
+def index():
+    return render_template('index.html')
