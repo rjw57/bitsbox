de185b11f32d4092de53d5eb7a79c5bfd3d3ec9e
Rich Wareham <rich@richwareham.com>
2017-01-20 10:13:10 +0000

    start to wire in relay

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 
 
 export default () => (
     <MuiThemeProvider>
       <div>
+      <AppBar title="hello"/>
       <div>hello</div>
       <RaisedButton label="test"/>
       </div>
